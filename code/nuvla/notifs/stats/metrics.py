573b245af869c700a9dd0eb15fc0e5918262949a
Amit Bhanja <amit.bhanja@sixsq.com>
2024-02-07 10:12:17 +0100

    Added prometheus monitoring
    - Defined metrics to be collected
    - Appropriate setting of metrics values
    - Exposed a port from where metrics would be
      scraped.
    - Added namespace to each metric

--- code/nuvla/notifs/stats/metrics.py
+++ code/nuvla/notifs/stats/metrics.py
@@ -1,21 +1,26 @@
 from prometheus_client import Counter, Gauge, Enum
+import __main__
 
-namespace = 'subs_notifs'
+if __main__.__file__.__contains__('monitoring'):
+    namespace = 'subs_notifs_monitoring'
+else:
+    namespace = 'subs_notifs'
 
-PROCESSING_TIME = Gauge(f'{namespace}_message_processing_time', 'Time spent in processing of a telemetry packet or '
+PROCESSING_TIME = Gauge('message_processing_time', 'Time spent in processing of a telemetry packet or '
                                                                 'event',
-                        ['type', 'message_info'])
+                        ['type', 'message_info'], namespace=namespace)
 
-PACKETS_PROCESSED = Counter(f'{namespace}_packets_processed', 'Number of telemetry packets processed',
-                            ['type', 'id'])
-PACKETS_ERROR = Counter(f'{namespace}_packets_error', 'Number of telemetry packets processed with error',
-                        ['type', 'id', 'exception'])
+PACKETS_PROCESSED = Counter('packets_processed', 'Number of telemetry packets processed',
+                            ['type', 'id'], namespace=namespace)
+PACKETS_ERROR = Counter('packets_error', 'Number of telemetry packets processed with error',
+                        ['type', 'id', 'exception'], namespace=namespace)
 
-SUBSCRIPTION_CONFIGS = Gauge(f'{namespace}_subscription_configs', 'Number of subscription configurations',
-                             ['type'])
+SUBSCRIPTION_CONFIGS = Gauge('subscription_configs', 'Number of subscription configurations',
+                             ['type'], namespace=namespace)
 
-PROCESS_STATES = Enum(f'{namespace}_process_states', 'State of the process',
-                      states=['idle', 'processing', 'error - recoverable', 'error - need restart'])
+PROCESS_STATES = Enum('process_states', 'State of the process',
+                      states=['idle', 'processing', 'error - recoverable', 'error - need restart'],
+                      namespace=namespace)
 
-ES_INDEX_DOCS_DELETED = Counter(f'{namespace}_es_index_docs_deleted', 'Number of documents deleted from an index',
-                                ['index', 'id'])
+ES_INDEX_DOCS_DELETED = Counter('es_index_docs_deleted', 'Number of documents deleted from an index',
+                                ['index', 'id'], namespace=namespace)
